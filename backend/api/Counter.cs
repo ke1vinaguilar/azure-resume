b8b52c9b55cdf96cc36d6ea5703c3add95547773
unknown <kelvinjaguilar@gmail.com>
2023-04-10 15:54:53 -0700

    Created azure function api

--- backend/api/Counter.cs
+++ backend/api/Counter.cs
@@ -4,10 +4,10 @@ namespace Company.Function
 {
     public class Counter
     {
-        [JsonProperty(PropertyName ="id")]
+        [JsonProperty(PropertyName="id")]
         public string Id {get; set;}
-
-        [JsonProperty(PropertyName ="count")]
-        public string Count {get; set;}
+        
+        [JsonProperty(PropertyName = "count")]
+        public int Count {get; set;}
     }
 }
